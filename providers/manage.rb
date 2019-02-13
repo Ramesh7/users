eca2c03350af9049716cfa4a328d5973f4d37175
Ramesh Sencha <ramesh.sencha@coupa.com>
2019-02-13 17:49:34 +0530

    CO-7470 : Adding support for time-based access for users

--- providers/manage.rb
+++ providers/manage.rb
@@ -56,7 +56,6 @@ action :create do
   else
     search(new_resource.data_bag, "groups:#{new_resource.search_group} AND NOT action:remove") do |u|
       u['username'] ||= u['id']
-      security_group << u['username']
 
       if node['apache'] and node['apache']['allowed_openids']
         Array(u['openid']).compact.each do |oid|
@@ -69,7 +68,7 @@ action :create do
       when 'mac_os_x'
           home_basedir = '/Users'
       when 'debian', 'rhel', 'fedora', 'arch', 'suse', 'freebsd'
-          home_basedir = '/home' 
+          home_basedir = '/home'
       end
 
       # Set home to location in data bag,
@@ -79,6 +78,12 @@ action :create do
       else
         home_dir = "#{home_basedir}/#{u['username']}"
       end
+
+      # calculate action for group and user resources
+      is_user_active = u['expiration_date'] ? Time.parse(u['expiration_date']) >= Time.now : true
+      is_action_create = u['action'] ? u['action'].eql?('create') : true
+      custom_action = is_user_active && is_action_create ? :create : :remove
+      security_group << u['username'] if custom_action.to_s.eql?('create')
 
       # The user block will fail if the group does not yet exist.
       # See the -g option limitations in man 8 useradd for an explanation.
@@ -108,52 +113,65 @@ action :create do
         action u['action'] if u['action']
       end
 
-	if home_dir != "/dev/null"
-	  converge_by("would create #{home_dir}/.ssh") do
-	    directory "#{home_dir}/.ssh" do
-	      owner u['username']
-	      group u['gid'] || u['username']
-	      mode "0700"
-	  end
-	end
+      next unless is_user_active && is_action_create
 
-        if u['ssh_keys']
-          template "#{home_dir}/.ssh/authorized_keys" do
-            source "authorized_keys.erb"
-            cookbook new_resource.cookbook
-            owner u['username']
-            group u['gid'] || u['username']
-            mode "0600"
-            variables :ssh_keys => u['ssh_keys']
-          end
+      if u['expiration_date']
+        template "#{home_dir}/.bash_profile" do
+          source "bash_profile.erb"
+          cookbook new_resource.cookbook
+          owner u['username']
+          group u['username']
+          mode "0644"
+          variables(remaining_time: (Time.parse(u['expiration_date']) - Time.now).to_i)
         end
+      end
 
-        if u['ssh_private_key']
-          key_type = u['ssh_private_key'].include?("BEGIN RSA PRIVATE KEY") ? "rsa" : "dsa"
-          template "#{home_dir}/.ssh/id_#{key_type}" do
-            source "private_key.erb"
-            cookbook new_resource.cookbook
-            owner u['id']
-            group u['gid'] || u['id']
-            mode "0400"
-            variables :private_key => u['ssh_private_key']
-          end
+    	if home_dir != "/dev/null"
+    	  converge_by("would create #{home_dir}/.ssh") do
+    	    directory "#{home_dir}/.ssh" do
+    	      owner u['username']
+    	      group u['gid'] || u['username']
+    	      mode "0700"
+    	  end
+    	end
+
+      if u['ssh_keys']
+        template "#{home_dir}/.ssh/authorized_keys" do
+          source "authorized_keys.erb"
+          cookbook new_resource.cookbook
+          owner u['username']
+          group u['gid'] || u['username']
+          mode "0600"
+          variables :ssh_keys => u['ssh_keys']
         end
+      end
 
-        if u['ssh_public_key']
-          key_type = u['ssh_public_key'].include?("ssh-rsa") ? "rsa" : "dsa"
-          template "#{home_dir}/.ssh/id_#{key_type}.pub" do
-            source "public_key.pub.erb"
-            cookbook new_resource.cookbook
-            owner u['id']
-            group u['gid'] || u['id']
-            mode "0400"
-            variables :public_key => u['ssh_public_key']
-          end
+      if u['ssh_private_key']
+        key_type = u['ssh_private_key'].include?("BEGIN RSA PRIVATE KEY") ? "rsa" : "dsa"
+        template "#{home_dir}/.ssh/id_#{key_type}" do
+          source "private_key.erb"
+          cookbook new_resource.cookbook
+          owner u['id']
+          group u['gid'] || u['id']
+          mode "0400"
+          variables :private_key => u['ssh_private_key']
+        end
+      end
+
+      if u['ssh_public_key']
+        key_type = u['ssh_public_key'].include?("ssh-rsa") ? "rsa" : "dsa"
+        template "#{home_dir}/.ssh/id_#{key_type}.pub" do
+          source "public_key.pub.erb"
+          cookbook new_resource.cookbook
+          owner u['id']
+          group u['gid'] || u['id']
+          mode "0400"
+          variables :public_key => u['ssh_public_key']
         end
       end
     end
   end
+end
 
   group new_resource.group_name do
     if new_resource.group_id
